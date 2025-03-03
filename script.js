2ecc63e2d5ff29ea3d4c61dbfed7a523369ed1f2
hackerman-101 <arshwaliaxyz@gmail.com>
2025-03-03 12:04:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,31 @@ function register() {
     let users = JSON.parse(localStorage.getItem("users")) || {};
 
     if (users[username]) {
-        message.innerText = "Username already registered! Try logging in.";
+        message.innerText = "This username is already registered. Please login.";
     } else {
         users[username] = password;
         localStorage.setItem("users", JSON.stringify(users));
         localStorage.setItem("currentUser", username);
         window.location.href = "stats.html";
+    }
+}
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
+
+    if (!username || !password) {
+        message.innerText = "Please enter both username and password!";
+        return;
+    }
+
+    let users = JSON.parse(localStorage.getItem("users")) || {};
+
+    if (users[username] && users[username] === password) {
+        localStorage.setItem("currentUser", username);
+        window.location.href = "stats.html";
+    } else {
+        message.innerText = "Invalid username or password!";
     }
 }
